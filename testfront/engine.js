d457353940718980a6494b5e958b2e4418774fcf
MikkeLUT <mikael.rehn@student.lut.fi>
2025-05-05 01:03:02 +0300

    First codes

---
+++ testfront/engine.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+button.addEventListener('click', () => {
+    console.log('Hello, World!');
+    const heading = document.querySelector('h1');
+    heading.textContent = 'Hello, World!';
+    
+});
